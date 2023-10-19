34af1247ac2520a44e9c5ca5653eb7536c48fc20
sharifulislamshihan <sharifulislamshihan@gmail.com>
2023-10-19 21:02:11 +0600

    login form added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,10 +3,32 @@ import Navbar from "../Shared/Navbar/Navbar";
 const Login = () => {
     return (
         <div>
-            <div className="my-4">
+            <div className="my-4 mb-10">
                 <Navbar></Navbar>
             </div>
-            <h2 className="text-2xl">Login page</h2>
+            <h2 className="text-3xl font-semibold text-center">Login your account</h2>
+            <div>
+                <form className="card-body md:w-3/4 lg:w-1/2 mx-auto">
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text font-semibold ">Email address</span>
+                        </label>
+                        <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text font-semibold">Password</span>
+                        </label>
+                        <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+                        <label className="label">
+                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                        </label>
+                    </div>
+                    <div className="form-control mt-6">
+                        <button className="btn btn-primary w-full">Login</button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
